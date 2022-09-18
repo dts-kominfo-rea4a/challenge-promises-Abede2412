a01b652bd3fddcdebe64d95f07751012c49cede1
Abede2412 <abdulrahman241296@gmail.com>
2022-09-18 15:40:19 +0800

    promise

--- promise.js
+++ promise.js
@@ -1,7 +1,48 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
-// TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+//TODO: Buat fungsi promiseOutput sesuai ketentuan readme
+const promiseOutput = (emosi) => {
+  return new Promise ((resolve, reject) => {
+    if (emosi != "marah" && emosi != "tidak marah"){
+      reject(" Emosi harus berupa \"marah\" atau \"tidak marah\"");
+    } else {
+      resolve(emosi);
+    }
+  })
+  .then(data => {
+    a = data;
+    return promiseTheaterIXX()
+  })
+  .then((array) =>{
+    array.forEach(element => {
+      if (element.hasil === a){
+        count += 1;
+      }
+    });
+    return promiseTheaterVGC()
+  })
+  .then((array) =>{
+    array.forEach(element => {
+      if (element.hasil === a){
+        count += 1;
+      }
+    });
+    return count;
+  })
+  .catch(error => console.log(error))
+  .finally(count = 0);
+
+};
+let a;
+let count = 0;
+
+  
+
+
+
+
+
+
 
 module.exports = {
   promiseOutput,
